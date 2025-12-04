274b60442e7524c5deaa59a877bc03c003b0b337
Maria Samos Rivas <samosrivas.m@nrotheastern.edu>
2025-12-04 00:10:06 -0500

    added all the mock data to the tables

--- database-files/weeklyMenu.sql
+++
@@ -1,4 +0,0 @@
-insert into weeklyMenu (menuID, recipeID) values (1, 5);
-insert into weeklyMenu (menuID, recipeID) values (4, 13);
-insert into weeklyMenu (menuID, recipeID) values (2, 3);
-insert into weeklyMenu (menuID, recipeID) values (2, 15);
